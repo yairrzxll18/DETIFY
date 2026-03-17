6f0213bbe7c5cfa8e9c9d97b334fe96d38e90207
yairrzxll18 <yairramirez241106@gmail.com>
2026-03-16 18:47:08 -0600

    fix mysql port railway

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 
 # Configuración MySQL
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
-app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT", 14605))
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DATABASE")
